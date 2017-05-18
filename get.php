19ef7d3642bb35ca88e9c0aeecad685a0e50296a
Ethan <lmly9193@gmail.com>
2017-05-19 00:01:18 +0800

    update

--- get.php
+++ get.php
@@ -1,10 +1,10 @@
 <?php
-	rewrite($_SERVER['QUERY_STRING']);
+	rewrite($_GET['id']);
 	function rewrite($id){
 		$url = strrev($id);
 		for($c=1;$c<=6;$c++){
 			$url = strrev(base64_encode($url));
 		}
 		$url = strrev($url);
-		header('Location: index.php?'.$url);
+		header('Location: /hash/'.$url);
 	}
--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-	if(empty($_SERVER['QUERY_STRING'])){
-		header('Location: error.html');	
-	}
-?>
 <?php require_once 'request.php';?>
 <!DOCTYPE html>
 <html>
@@ -12,12 +7,12 @@
 		<meta name='viewport' content='width=device-width'>
 		<link rel='icon' href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAA3NCSVQICAjb4U/gAAAACXBIWXMAAABvAAAAbwHxotxDAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAFFQTFRF////AAAAGwAALQEBPgICOAICSCEhTwMDUkVFUkZGVD4+VQMDXx0dghERkgUFmQUFmo+PnpaWogUFpQUFrgYGuQYGugYG0wcH1QcH6Ojo////VIe8EAAAAAZ0Uk5TAAhV1OLk46qZIwAAAFJJREFUGFdjYMAEjEgAyGViZkECzEwMzBIogJmBFUwLiUMFWCECwhxsoigC/IKSXLyoAlJSAjz4BdC0CHOiGYpsLdxhfOzs3CJizJhOx/AcOgAA5zwM2aaHDyMAAAAASUVORK5CYII='>
 		<script src='//content.jwplatform.com/libraries/md7nkykN.js'></script>
-		<link rel='stylesheet' href='css/normalize.css'>
-		<link rel='stylesheet' href='css/tube.css'>
+		<link rel='stylesheet' href='/css/normalize.css'>
+		<link rel='stylesheet' href='/css/tube.css'>
 	</head>
 	<body>
 		<div id='videoContainer'>Loading Error...</div>
-		<script><?php d($_SERVER['QUERY_STRING'])?></script>
+		<script><?php d($_GET['lmly9193'])?></script>
 		<noscript>您的瀏覽器不被網站支援</noscript>
 	</body>
 </html>
